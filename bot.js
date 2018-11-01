441949723183e41e97fc7386bcb2257b2cee43da
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-01 21:07:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,15 @@ var prefix = "-N"
 client.on('message', function(message) {
 	const myID = "416643173239226388";
     let args = message.content.split(" ").slice(1).join(" ");
-    if(message.content.startsWith(prefix + "s")) {
+    if(message.content.startsWith(prefix + "sn")) {
+		        if(message.author.id !== myID) return;
+            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+        client.user.setUsername(args);
+        message.channel.send('**SetName,Done!**').then(msg => {
+           msg.delete(5000);
+          message.delete(5000);
+        });
+    } else if(message.content.startsWith(prefix + "s")) {
 		        if(message.author.id !== myID) return;
             if(!args) return message.reply('اكتب الحالة اللي تريدها.');
         client.user.setGame(args , 'https://twitch.tv/6xlez1');
@@ -52,4 +60,6 @@ client.on('message', function(message) {
         });
     }
 });
-client.login(process.env.BOT_TOKEN);  
+
+
+client.login(process.env.BOT_TOKEN); 
